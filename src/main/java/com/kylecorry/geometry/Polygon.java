b4df1a94042922f154d4fd598f67dfa574ac7b44
Kyle Corry <kylecorry31@gmail.com>
2017-02-26 12:18:09 -0500

    Add Polygon equals method

--- src/main/java/com/kylecorry/geometry/Polygon.java
+++ src/main/java/com/kylecorry/geometry/Polygon.java
@@ -1,5 +1,7 @@
 package com.kylecorry.geometry;
 
+import java.util.Arrays;
+
 /**
  * A representation of a polygon in 3D space, where the first and last
  * points are assumed to be connected.
@@ -14,5 +16,13 @@ public class Polygon {
      */
     public Polygon(Point[] points) {
         this.points = points;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Polygon))
+            return false;
+        Polygon other = (Polygon) obj;
+        return Arrays.equals(points, other.points);
     }
 }
