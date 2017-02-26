39a56be9c8b03dfdf0d3d6cf3d0d62d3701b5ff7
Kyle Corry <kylecorry31@gmail.com>
2017-02-26 11:26:50 -0500

    Create Pose2D class

---
+++ src/main/java/com/kylecorry/geometry/Pose2D.java
@@ -0,0 +1,30 @@
+package com.kylecorry.geometry;
+
+/**
+ * A position and orientation in 2D space.
+ */
+public class Pose2D {
+    public double x, y, theta;
+
+    /**
+     * Create a pose in 2D space.
+     *
+     * @param x     The x coordinate.
+     * @param y     They y coordinate.
+     * @param theta The orientation in radians.
+     */
+    public Pose2D(double x, double y, double theta) {
+        this.x = x;
+        this.y = y;
+        this.theta = theta;
+    }
+
+    /**
+     * Create a pose in 3D space from this 2D pose.
+     *
+     * @return This pose in 3D space.
+     */
+    public Pose to3DPose() {
+        return new Pose(new Point(x, y, 0), new Quaternion(theta, Vector3.k));
+    }
+}
