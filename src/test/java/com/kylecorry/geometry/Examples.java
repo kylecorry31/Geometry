fe67273c7f21697c29b36afb263b264045165cd8
Kyle Corry <kylecorry31@gmail.com>
2017-02-26 11:29:18 -0500

    Add test cases for Pose2D

--- src/test/java/com/kylecorry/geometry/Examples.java
+++ src/test/java/com/kylecorry/geometry/Examples.java
@@ -70,10 +70,17 @@ public class Examples {
 
     @Test
     public void testPose() {
-        Pose pose = new Pose(new Point(1, 2, 3), new Quaternion(90, Vector3.k));
-
+        Pose pose = new Pose(new Point(1, 2, 3), new Quaternion(Math.PI / 2, Vector3.k));
         assertEquals(new Point(1, 2, 3), pose.position);
-        assertEquals(new Quaternion(90, Vector3.k), pose.orientation);
+        assertEquals(new Quaternion(Math.PI / 2, Vector3.k), pose.orientation);
+    }
+
+    @Test
+    public void testPose2D() {
+        Pose2D pose2D = new Pose2D(1, 2, Math.PI / 2);
+        assertEquals(1, pose2D.x, 0);
+        assertEquals(2, pose2D.y, 0);
+        assertEquals(Math.PI / 2, pose2D.theta, 0);
     }
 
     @Test
@@ -86,8 +93,8 @@ public class Examples {
         assertEquals(new Quaternion(-Math.PI / 2, Vector3.k), quaternion.inverse());
         assertEquals(new Quaternion(0, Vector3.k), quaternion1.inverse());
         assertEquals(new Quaternion(Math.PI / 2, Vector3.k), quaternion.multiply(quaternion1));
-        assertEquals(new Quaternion(1/18.0, 1/9.0, -1/6.0, -1/9.0), new Quaternion(1, -2, 3, 2).inverse());
-        assertEquals(new Quaternion(11/129.0, 2/129.0, 0, 2/129.0), new Quaternion(11, -2, 0, -2).inverse());
+        assertEquals(new Quaternion(1 / 18.0, 1 / 9.0, -1 / 6.0, -1 / 9.0), new Quaternion(1, -2, 3, 2).inverse());
+        assertEquals(new Quaternion(11 / 129.0, 2 / 129.0, 0, 2 / 129.0), new Quaternion(11, -2, 0, -2).inverse());
         assertEquals(new Quaternion(11, -30, 25, 26), new Quaternion(1, -2, 3, 2).multiply(new Quaternion(11, -2, 0, -2)));
         assertEquals(new Quaternion(-450, 628, 302, 22), new Quaternion(9, 22, 4, 6).multiply(new Quaternion(18, 28, 2, -2)));
         assertTrue(pointApproxEqual(new Point(1, 0, 0), quaternion.rotate(new Point(0, -1, 0)), EPSILON));
