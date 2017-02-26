965d4c2ed04f4def7b4441e1e2eccf0d061469b1
Kyle Corry <kylecorry31@gmail.com>
2017-02-26 11:39:05 -0500

    Create Accel

---
+++ src/main/java/com/kylecorry/geometry/Accel.java
@@ -0,0 +1,19 @@
+package com.kylecorry.geometry;
+
+/**
+ * Represents linear and angular acceleration in 3D space.
+ */
+public class Accel {
+    public Vector3 linear, angular;
+
+    /**
+     * Creates a 3D acceleration.
+     *
+     * @param linear  The linear acceleration component.
+     * @param angular The angular acceleration component.
+     */
+    public Accel(Vector3 linear, Vector3 angular) {
+        this.linear = linear;
+        this.angular = angular;
+    }
+}
